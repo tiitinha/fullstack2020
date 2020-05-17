632eae286d44e7737530f07b5fd0a347ad0d6dc7
afroseppo <kobo-kobe@hotmail.com>
2020-05-17 13:31:23 +0300

    2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
 
   const [ newName, setNewName ] = useState('')
 
@@ -19,6 +14,18 @@ const App = () => {
 
   const [ filterValue, setFilterValue ] = useState('')
 
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
+  console.log(persons)
 
   const addPerson = (event) => {
     event.preventDefault()
